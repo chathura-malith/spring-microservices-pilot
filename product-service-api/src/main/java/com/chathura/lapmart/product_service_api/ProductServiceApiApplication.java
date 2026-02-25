b0c60f6f6ef2f6a82b8ea190a8b9a8c9030e9e4b
Chathura-Malith <153283861+chathurama@users.noreply.github.com>
2026-02-25 15:23:48 +0530

    Corrected monorepo project structure

---
+++ product-service-api/src/main/java/com/chathura/lapmart/product_service_api/ProductServiceApiApplication.java
@@ -0,0 +1,15 @@
+package com.chathura.lapmart.product_service_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ProductServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApiApplication.class, args);
+	}
+
+}
